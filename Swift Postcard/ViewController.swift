bf6ceb798d532a6787e124c909940386f4a10a8b
BrainStarr <brian.r.starr@gmail.com>
2014-09-23 17:02:27 -0700

    added in some nav functions, completed first code challenge

--- Swift Postcard/ViewController.swift
+++ Swift Postcard/ViewController.swift
@@ -10,14 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageStatusLabel: UILabel!
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var messageTextField: UITextField!
+    
+    @IBOutlet weak var mailButton: UIButton!
+    
+    @IBOutlet weak var hiddenChallengeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func sendPostcardButton(sender: AnyObject) {
+        messageStatusLabel.hidden = false
+        messageStatusLabel.text = messageTextField.text
+        messageTextField.resignFirstResponder()
+        messageStatusLabel.textColor = UIColor.redColor()
+        mailButton.setTitle("Mail Sent", forState:UIControlState.Normal)
+        hiddenChallengeLabel.hidden = false
+        hiddenChallengeLabel.text = messageTextField.text
+        hiddenChallengeLabel.textColor = UIColor.blueColor()
+        messageTextField.text = ""
     }
 
 
